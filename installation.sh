9dae08a4c0dc91bbefc6a592ba6e6ad9b1747793
Leif Seute <leif.seute@gmx.de>
2024-05-06 16:41:54 +0200

    .

--- installation.sh
+++ installation.sh
@@ -1,19 +1,23 @@
+#!/bin/bash
+
+# Default to "cpu" if no argument provided, otherwise use the provided CUDA version
 cuda_version=${1:-"cpu"}
 
-# check that we are not in base environment:
+# Check that we are not in the base Conda environment
 if [ "$CONDA_DEFAULT_ENV" == "base" ]; then
-    echo "Please activate a conda environment before running this script."
+    echo "Please activate a Conda environment before running this script."
     exit 1
 fi
 
 echo "Installing log_p_gnn in environment: $CONDA_DEFAULT_ENV"
 
-# if not cpu mode:
-# conda install python=3.10 pytorch=2.1.0 dgl -c pytorch -c dglteam -y
-
-
-# install for cuda 11.8 (other versions usually make problems with dgl...)
-conda install python=3.10 pytorch=2.1.0 pytorch-cuda=11.8 dgl -c pytorch -c nvidia -c dglteam/label/cu118 -y
+# If not in CPU mode, install with CUDA support
+if [ "$cuda_version" != "cpu" ]; then
+    conda install python=3.10 pytorch=2.1.0 dgl -c pytorch -c dglteam -y
+else
+    # install for cuda 11.8 (other versions usually make problems with dgl...)
+    conda install python=3.10 pytorch=2.1.0 pytorch-cuda=11.8 dgl -c pytorch -c nvidia -c dglteam/label/cu118 -y
+fi
 
 pip install -r requirements.txt
 pip install -e .
